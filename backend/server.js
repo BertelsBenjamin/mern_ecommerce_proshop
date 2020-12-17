ad77d58e953594ecf1e9b9c1f77efe603ba7d107
Benjamin Bertels <bertels123@gmail.com>
2020-12-17 06:52:34 +0100

    paypal

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import connectDB from './config/db.js';
 import colors from 'colors';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
@@ -15,6 +16,11 @@ app.use(express.json()); //allows using json in body of POST requests
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
 
 app.use(notFound);
 
