6e8b662ec2fb3160e3be851c69a7818e3d4a0f34
Benjamin Bertels <bertels123@gmail.com>
2020-12-17 07:36:57 +0100

    listMyOrders typo fix

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -81,7 +81,7 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
 // @route   GET /api/orders/myorders
 // @access  Private
 const getMyOrders = asyncHandler(async (req, res) => {
-  const orders = await Order.find(req.user._id);
+  const orders = await Order.find({ user: req.user._id });
 
   res.json(orders);
 });
